2bbedafd9bb15aaf990fc0afaab50d086627275b
Paul Carlton <paul.carlton@weave.works>
2023-11-02 10:20:00 +0000

    Remove vault oidc

--- resources/github-secrets-sample.sh
+++ resources/github-secrets-sample.sh
@@ -1,7 +1,6 @@
 GITHUB_CLIENT_ID=... # Github Organisation Client ID
 GITHUB_CLIENT_SECRET=... # Github Organisation Client Secret
 WGE_DEX_CLIENT_SECRET="$(date +%s | sha256sum | base64 | head -c 10)"
-VAULT_DEX_CLIENT_SECRET="$(date +%s | sha256sum | base64 | head -c 10)"
 
 export GITHUB_TOKEN=... # write access token
 
